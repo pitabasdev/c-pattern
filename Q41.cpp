b1e5a2cbb6d64f3121f78a09b233c25fbc61bb0b
pitabas1977 <85897297+pitabas1977@users.noreply.github.com>
2023-08-19 20:27:23 +0530

    question update

---
+++ Q41.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    for(int row=0;row<n;row++){
+        for(int col=row+1;col<=n;col++){
+            if(col==row+1 || col==n || row==0){
+                cout<<col;
+            }else{
+                cout<<" ";
+            }
+        }
+        cout<<endl;
+    }
+}
